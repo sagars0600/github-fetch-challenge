4df52c7b4c7a09d63b0514a2a68c971d8ac67a21
Sagar Solanki <59771375+sagars0600@users.noreply.github.com>
2022-03-19 18:56:05 +0530

    first commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("./dist/angular-app-heroku"));
+app.use(express.static("./dist/github-fyle"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-app-heroku/" })
+  res.sendFile("index.html", { root: "dist/github-fyle/" })
 );
 
 app.listen(process.env.PORT || 8080);
